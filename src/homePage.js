41737a6dbb8cf85cc4a25dc87065f6ff9edfe173
Harshal Shirote <harshalshirote2002@gmail.com>
2023-08-29 11:35:24 +0530

    add task dialog module

--- src/homePage.js
+++ src/homePage.js
@@ -1,3 +1,38 @@
+import createDialog from './taskDialog.js';
+
+function taskMaker(title, description, dueDate, priority, notes, check){
+    return {title, description, dueDate, priority, notes, check};
+}
+
+function callDialog(){
+    const dialog = document.querySelector('dialog');
+    dialog.showModal();
+    const dialogWidth = dialog.offsetWidth;
+    const dialogHeight = dialog.offsetHeight;
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+    const leftPosition = viewportWidth / 2 - dialogWidth / 2;
+    const topPosition = viewportHeight / 2 - dialogHeight / 2;
+    dialog.style.left = `${leftPosition}px`;
+    dialog.style.top = `${topPosition}px`;
+}
+
+function viewDropdown(e){
+    const allTasks = Array.from(document.querySelectorAll('.task'));
+    allTasks.forEach((task) =>  {
+        if(Array.from(task.classList).includes('task-dropdown-active')){
+            if(!(e.target==task)){
+                task.classList.remove('task-dropdown-active');
+            }
+        }
+    });
+    if(Array.from(e.target.classList).includes('task-dropdown-active')){
+        e.target.classList.remove('task-dropdown-active');
+    }else{
+        e.target.classList.add('task-dropdown-active');
+    }
+}
+
 export default function createHome(){
     const main = document.createElement('main');
     const sidebar = document.createElement('div');
@@ -9,13 +44,15 @@ export default function createHome(){
     const addTask = document.createElement('button');
     sidebar.classList.add('sidebar');
     sidebarHead.classList.add('sidebar-header');
-    sidebarHead.textContent = 'Menu';
+    sidebarHead.textContent = 'John Doe';
     sidebarContent.classList.add('sidebar-content');
     taskHolder.classList.add('task-holder');
     taskHead.classList.add('task-header');
     taskHead.textContent = 'Tasks';
-    taskContent.classList.add('task-content');
+    taskContent.classList.add('tasks-container');
     addTask.classList.add('add-task')
+    addTask.textContent = 'Add a task';
+    addTask.addEventListener('click', callDialog);
 
     sidebar.appendChild(sidebarHead);
     taskHolder.appendChild(taskHead);
@@ -30,7 +67,7 @@ export default function createHome(){
         sidebarContent.appendChild(space);
     }
 
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 4; i++) {
         const task = document.createElement('div');
         const marker = document.createElement('button');
         const content = document.createElement('div');
@@ -39,6 +76,7 @@ export default function createHome(){
         const description = document.createElement('p');
         const deadline = document.createElement('p');
         const priority = document.createElement('p');
+        const notes = document.createElement('div');
         task.classList.add('task');
         task.classList.add(`${i}`);
         marker.classList.add(`task-marker`);
@@ -48,18 +86,22 @@ export default function createHome(){
         parameters.classList.add('task-parameters');
         deadline.classList.add('task-deadline');
         priority.classList.add('task-priority');
+        notes.classList.add('task-notes');
         title.textContent = 'title';
         description.textContent = 'description';
         deadline.textContent = 'd...';
         priority.textContent = 'p...';
+        notes.textContent = 'lorem ipsum dolor sasdsada ff erf df f fd d h h hygy fg hghgh ggggg ggggg ggggg ggggg gggg h h hjhj lorem ipsum dolor sasdsada ff erf df f fd d h h hygy fg hghgh ggggg ggggg ggggg ggggg gggg h h hjhj lorem ipsum dolor sasdsada ff erf df f fd d h h hygy fg hghgh ggggg ggggg ggggg ggggg gggg h h hjhj';
         content.append(title, description);
         parameters.append(deadline, priority);
-        task.append(marker, content, parameters);
-        taskHolder.appendChild(task);
+        task.append(marker, content, parameters, notes);
+        task.addEventListener('click', viewDropdown);
+        taskContent.appendChild(task);
     }
-
-    
+    taskHolder.appendChild(taskContent);
+    taskHolder.appendChild(addTask);
     sidebar.appendChild(sidebarContent);
-    main.append(sidebar, taskHolder, addTask);
+    const dialog = createDialog();
+    main.append(sidebar, taskHolder, dialog);
     return main;
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import createHeader from "./commonHeader.js";
 import createHome from "./homePage.js";
 import createFooter from "./commonFooter.js";
 import './styles.css';
 
-const header = createHeader();
 const main = createHome();
 const footer = createFooter();
 const body = document.querySelector('body');
-body.appendChild(header);
 body.appendChild(main);
-body.appendChild(footer);
